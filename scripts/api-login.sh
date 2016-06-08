c33d1f1fdc343bd281e8c0a667151d52e024ff72
R. S. Doiel <rsdoiel@gmail.com>
2016-06-08 11:18:14 -0700

    Adding comments

---
+++ scripts/api-login.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Not implemented yet. This script will display the ORCID_ACCESS_TOKEN based on authenticating against the API. You will then need to set that value as the environment vairable ORCID_ACCESS_TOKEN to use the other scripts in this directory."
---
+++ scripts/api-search-email.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Not implemented yet. This script will search for an ORCID by providing a partial or complete email address."
